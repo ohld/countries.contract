d789d08034fef583c85175d166bb4785d60064ad
Aleksey Bykhun <theisease@gmail.com>
2018-07-16 10:53:30 +0300

    + [NPM INSTALL] openzeppelin imports

--- contracts/CountryToken.sol
+++ contracts/CountryToken.sol
@@ -1,2 +1,39 @@
-// should be taken and changed using this code:
-// https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC721/ERC721Token.sol
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+
+contract CountryToken is ERC721Token {
+
+  struct Country {
+    uint256 countryId;
+    uint256 createdAt;
+    uint256 lastPrice;
+  }
+
+  mapping (uint256 => Country) map;
+
+  constructor (string _name, string _symbol) public
+    ERC721Token(_name, _symbol) {
+  }
+
+  function createCountry(uint256 _id) external {
+    //require(dont exist);
+    // better mint to nobody owning
+
+    _mint(msg.sender, _id);
+
+    map[_id] = Country(
+      _id,
+      now,
+      uint256(0)
+    );
+  }
+
+  function getCountryLastPrice(uint256 countryId) public view returns (uint256 price) {
+    // require(exists);
+    return map[countryId].lastPrice;
+  }
+
+
+
+}
--- contracts/WorldMap.sol
+++ contracts/WorldMap.sol
@@ -1,44 +1,49 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-zos/contracts/ownership/Ownable.sol";
-import "openzeppelin-zos/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-import ".CountryToken.sol"
+import "./CountryToken.sol";
 
 // inspired with: https://github.com/zeppelinos/basil/blob/master/contracts/BasilERC721.sol
 
 contract WorldMap is Ownable {
     using SafeMath for uint256;
 
-    uint8 DEFAULT_COLOR = 0; // ????????
+    bytes12 DEFAULT_COLOR = 0xffffff; // ????????
     string DEFAULT_TEXT = ""; // ?????????
 
-    // ERC721 non-fungible tokens to be emitted on donations.
-    ERC721Token public token; // countries
-    uint256 public numEmittedTokens; // will also use as next id for new country
+    address TOKEN_ADDRESS = 0x17dA6A8B86578CEC4525945A355E8384025fa5Af;
 
-    function setToken(ERC721Token _token) external onlyOwner {
-        // initial thing to set token (see link above)
-        // I don't know the best practices now, this function may be changed
-        require(_token != address(0));
-        require(token == address(0));
-        token = _token;
+    // ERC721 non-fungible tokens to be emitted on donations.
+    CountryToken public map; // countries
+    /* uint256 public numEmittedTokens; // will also use as next id for new country */
+
+    constructor() public {
+        map = CountryToken(TOKEN_ADDRESS);
+    }
+
+    function setToken(uint256 _id) external onlyOwner {
+        map.createCountry(_id);
     }
 
     function buy(uint256 countryId) public payable {
-        // check msg.value > countryId.price
+        require(msg.value > map.getCountryLastPrice(countryId));
+
         _transferCountry(countryId, msg.sender);
     }
 
     function _transferCountry(uint256 countryId, address _newOwner) internal {
-        // token.transfer(countryId, _newOwner);
+        /* address currOwner = map.ownerOf(countryId); */
+        map.safeTransferFrom(_newOwner, map.ownerOf(countryId), countryId);
     }
 
-    function createCountry(string name) external onlyOwner { // external or public?
+    /* function createCountry(string name) external onlyOwner {
+        // external or public?
         // token.mint(address(0), numEmittedTokens); // or which address use?
         // token._setCountryName(numEmittedTokens, name);
         // token._setCountryColor(DEFAULT_COLOR);
         // token._setCountryText(DEFAULT_TEXT);
         // numEmittedTokens = numEmittedTokens.add(1);
-    }
+    } */
 }
