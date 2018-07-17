c3a6080740d4a8cd99cf781cb212fb28d500266d
Aleksey Bykhun <theisease@gmail.com>
2018-07-17 12:19:23 +0300

    + add feeless.sol, not yet used

---
+++ contracts/Feeless.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.24;
+
+import { ECRecovery } from "openzeppelin-solidity/contracts/ECRecovery.sol";
+
+contract Feeless {
+
+    address internal msgSender;
+    mapping(address => uint256) public nonces;
+
+    modifier feeless {
+        if (msgSender == address(0)) {
+            msgSender = msg.sender;
+            _;
+            msgSender = address(0);
+        } else {
+            _;
+        }
+    }
+
+    function performFeelessTransaction(address sender, address target, bytes data, uint256 nonce, bytes sig) public payable {
+        require(this == target);
+
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 payload = keccak256(abi.encodePacked(target, data, nonce));
+        bytes32 hash = keccak256(abi.encodePacked(prefix, payload));
+        msgSender = ECRecovery.recover(hash, sig);
+        require(msgSender == sender);
+        require(nonces[msgSender]++ == nonce);
+
+        require(target.call.value(msg.value)(data));
+        msgSender = address(0);
+    }
+
+}
